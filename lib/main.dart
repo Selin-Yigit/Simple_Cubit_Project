9fe091b642afc58be26964223eec4eb1444eecb2
Selin Yigit <sln.ygt.41@gmail.com>
2023-11-28 17:00:28 +0300

    Proje tamamlandı.

--- lib/main.dart
+++ lib/main.dart
@@ -27,23 +27,45 @@ extension RandomElement<T> on Iterable<T> {
       );
 }
 
-class NamesCubits extends Cubit<String?>{
+class NamesCubits extends Cubit<String?> {
   // Cubit bir initial state almak zorundadır. Yani başlangıçta bir değerin olup olmadığı
-  // Burada konrol edilmelidir. Bizim başlangıçta bir değerimiz olmayacağı için 
+  // Burada konrol edilmelidir. Bizim başlangıçta bir değerimiz olmayacağı için
   // Super içine "null" verdik.
   NamesCubits() : super(null);
-  
-  void pickRandomName(){
+
+  void pickRandomName() {
     // Burada state'in sadece getter'ı bulunur ve Cubit'in o anki durumunu getirir.
-    // Yani state'e yeni bir durum verilemez Çünkü setter bulunmaz. 
+    // Yani state'e yeni bir durum verilemez Çünkü setter bulunmaz.
     // Yeni bir state üretmek için emit fonksiyonu kullanılır. Bu sayede yeni durumlar
     // ve değerler üretilebilir.
     emit(names.getRandomElement());
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  // Normalde Stateless olarak oluşturduğumuz HomePage'i Statefull hale getirdik.
+  // Çünkü Cubit dispose edilmesi gereken bir yapıdır. Stateful ile birlikte
+  // initialize ve dispose işlemlerini yapabiliriz.
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final NamesCubits cubit;
+
+  @override
+  void initState() {
+    super.initState();
+    cubit = NamesCubits();
+  }
+
+  @override
+  void dispose() {
+    cubit.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +78,45 @@ class HomePage extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Center(
+        child: StreamBuilder<String?>(
+          stream: cubit.stream,
+          builder: (context, snapshot) {
+            final button = TextButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStatePropertyAll(Colors.lightGreen.shade400),
+              ),
+              onPressed: () {
+                cubit.pickRandomName();
+              },
+              child: const Text(
+                "Pick a random name!",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return button;
+              case ConnectionState.waiting:
+                return button;
+              case ConnectionState.active:
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(snapshot.data ?? ''),
+                    button,
+                  ],
+                );
+              case ConnectionState.done:
+                return const SizedBox();
+            }
+          },
+        ),
+      ),
     );
   }
 }
